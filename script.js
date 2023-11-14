52b425f291634c76bbb57c11394244b17e983341
blu3tan <142602465+blu3tan@users.noreply.github.com>
2023-11-14 23:07:05 +0100

    Add javascript password comparison

---
+++ script.js
@@ -0,0 +1,37 @@
+
+const pwd = document.getElementById('password');
+const pwdcheck = document.getElementById('password-check');
+const check = document.getElementById('check');
+
+pwdcheck.addEventListener('input', () => {
+    compare();
+});
+
+pwd.addEventListener('input', () => {
+    comparePwdAfter();
+});
+
+function compare() {
+    if (!pwdcheck.value) {
+        check.innerHTML = '';
+        check.classList.remove('compare');
+    }
+    else if (pwd.value !== pwdcheck.value) {
+        check.classList.add('compare');
+        check.classList.remove('toggle');
+        check.innerHTML = 'Please enter the same password';
+    }
+    else {
+        check.innerHTML = '';
+        check.classList.remove('compare');
+    }
+};
+
+// This function fix the case when users corrects the password-check field before the password fits the given pattern
+
+function comparePwdAfter() {
+    if (pwd.value !== pwdcheck.value) {
+        check.innerHTML = '';
+        check.classList.remove('compare');
+    }
+}
